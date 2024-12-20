bffc6320a956be9bd962c50e20d8ba7ee017df82
ShragSir <162401234+sirhorseyinfo@users.noreply.github.com>
2024-12-20 16:21:32 +0000

    Remote from codespaces

---
+++ radio/station/909.sh
@@ -0,0 +1,46 @@
+#intro1=""
+#intro2=""
+#outro1=""
+#outro2=""
+
+station_logo() {
+  echo "(logo)"
+}
+
+rotate() {
+  randnum=$((RANDOM % 8 + 1))
+  rotation_var="rotation_$randnum"
+  case $randnum in
+    1)
+      echo "Executing task for rotation 1"
+      ;;
+    2)
+      echo "Executing task for rotation 2"
+      ;;
+    3)
+      echo "Executing task for rotation 3"
+      ;;
+    4)
+      echo "4"
+      ;;
+    5)
+      echo "5"
+      ;;
+    6)
+      echo "6"
+      ;;
+    7)
+      echo "7"
+      ;;
+    8)
+      echo "8"
+      ;;
+    *)
+      echo "[-!-]: Unexpected value: $randnum"
+      ;;
+    esac
+}
+
+rotate
+
+exit
